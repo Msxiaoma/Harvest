9ed03e94c2a46c3a58e38e586c92dfd8d89a507e
maai <xiaoma961211@163.com>
2018-07-17 19:23:12 +0800

    picture

---
+++ src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <picture-harvest :imgArr="imgArr"></picture-harvest>
+    </div>
+</template>
+
+<script>
+    import pictureHarvest from './picture.vue'
+    export default {
+        components: {
+            pictureHarvest
+        },
+        data () {
+            return {
+                imgArr: [require('../assets/img/6.jpeg'),require('../assets/img/1.jpeg'), require('../assets/img/2.jpeg'), require('../assets/img/3.jpeg'), require('../assets/img/4.jpeg'), require('../assets/img/5.jpeg'), require('../assets/img/6.jpeg'),require('../assets/img/1.jpeg')]
+            }
+        }
+    }
+</script>
+
---
+++ src/components/picture.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="harvest">
+        <!-- 图片轮播 -->
+        <img :src="imgArr[currrentIndex-1]" class="box-left-img" width="243" height="380">
+        <img :src="imgArr[currrentIndex]" class="box-center-img" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd" width="243" height="380">
+        <img :src="imgArr[currrentIndex+1]" class="box-right-img" width="243" height="380">
+    </div>
+</template>
+
+<script>
+    export default {
+        created() {
+            // 适配移动设备
+           
+        },
+        props: {
+            imgArr: Array
+        },
+        data() {
+            return {
+                leftIndex: 5,
+                currrentIndex: 1,
+                rightIndex:1,
+                moveX: 0,
+                moveY: 0,
+                endX: 0,
+                endY: 0,
+                pictureStyle: ''
+            }
+        },
+        mounted() {
+            
+        },
+        computed: {
+            changeArr() {
+                return this.imgArr
+            }
+        },
+        watch: {
+            currrentIndex(newVal, oldVal) {
+                if(newVal == 7) {
+                    this.currrentIndex = 1
+                } 
+                if (newVal == 0) {
+                    this.currrentIndex = 6
+                }
+            }
+        },
+        methods: {
+            touchStart(e) {
+                this.moveX = e.targetTouches[0].pageX
+                this.moveY = e.targetTouches[0].clientHeight
+            },
+            touchMove(e) {
+                 this.endX = e.targetTouches[0].pageX - this.moveX
+                 this.endY = e.target.clientWidth - this.moveY
+            },
+            touchEnd() {
+                if (this.endX > 40) {
+                    console.log('右滑')
+                    this.currrentIndex--
+                    this.pictureStyle =  ''
+                } 
+                if(this.endX < -40) {
+                    console.log('左滑')
+                    this.currrentIndex++
+                    this.pictureStyle =  'animated fadeInRight'
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    body {
+        background: #e2e2e257;
+        overflow: hidden;
+    }
+    
+    .harvest {
+        margin-top: 2rem;
+        height: 10rem;
+    }
+    
+    .box-center-img {
+        position: relative;
+        height: 8.2rem;
+        width: 6.3rem;
+        background: burlywood;
+        display: inline-block;
+        border-radius: 0.2rem;
+        animation: fade-in;/*动画名称*/  
+        animation-duration: 1.5s;/*动画持续时间*/  
+        -webkit-animation:fade-in 1.5s;/*针对webkit内核*/  
+    }
+    
+    .box-left-img {
+        height: 7.6rem;
+        float: left;
+        border-top-right-radius: 0.2rem;
+        border-bottom-right-radius: 0.2rem;
+        margin-left: -4.5rem;
+        margin-right: 0.2rem;
+        margin-top: 0.3rem;
+    }
+    
+    .box-right-img {
+        background: red;
+        height: 7.6rem;
+        float: right;
+        border-top-left-radius: 0.2rem;
+        border-bottom-left-radius: 0.2rem;
+        margin-left: 0.2rem;
+        margin-top: 0.3rem;
+        margin-right: -4.45rem;
+    }
+</style>
+
+
